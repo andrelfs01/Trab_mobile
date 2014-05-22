f100b81fba1963e269122fee3996cd5b2cf0c322
andrelfs01 <andre.luisfs01@gmail.com>
2014-05-22 20:20:49 -0300

    trocando para o planning poker

--- Trabalho_Moveis/gen/br/trabalho_moveis/R.java
+++ Trabalho_Moveis/gen/br/trabalho_moveis/R.java
@@ -24,24 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080002;
-        public static final int detalhes=0x7f080003;
-        public static final int nome=0x7f080000;
-        public static final int salvar=0x7f080001;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_nova_tarefa=0x7f030000;
-        public static final int activity_tarefas=0x7f030001;
+        public static final int activity_login=0x7f030000;
     }
     public static final class menu {
-        public static final int nova__tarefa=0x7f070000;
-        public static final int tarefas_=0x7f070001;
+        public static final int login_=0x7f070000;
+        public static final int nova__tarefa=0x7f070001;
+        public static final int tarefas_=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_login_=0x7f050004;
         public static final int title_activity_nova__tarefa=0x7f050003;
     }
     public static final class style {
