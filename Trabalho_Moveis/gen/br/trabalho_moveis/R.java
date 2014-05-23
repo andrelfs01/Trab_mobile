5954eb102564dc3746b2f2b19723926f049f6cf6
andrelfs01 <andre.luisfs01@gmail.com>
2014-05-23 20:20:23 -0300

    Commit 1

--- Trabalho_Moveis/gen/br/trabalho_moveis/R.java
+++ Trabalho_Moveis/gen/br/trabalho_moveis/R.java
@@ -24,20 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000a;
+        public static final int buttonAdd=0x7f080003;
+        public static final int buttonConcluir=0x7f080004;
+        public static final int criar=0x7f080009;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
+        public static final int editTextNomePlanning=0x7f080006;
+        public static final int editTextSenhaPlanning=0x7f080007;
+        public static final int entrar=0x7f080008;
+        public static final int imageView1=0x7f080005;
+        public static final int listView1=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
+        public static final int activity_criar=0x7f030000;
+        public static final int activity_login=0x7f030001;
     }
     public static final class menu {
-        public static final int login_=0x7f070000;
-        public static final int nova__tarefa=0x7f070001;
-        public static final int tarefas_=0x7f070002;
+        public static final int criar=0x7f070000;
+        public static final int login_=0x7f070001;
+        public static final int nova__tarefa=0x7f070002;
+        public static final int tarefas_=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_criar=0x7f050005;
         public static final int title_activity_login_=0x7f050004;
         public static final int title_activity_nova__tarefa=0x7f050003;
     }
