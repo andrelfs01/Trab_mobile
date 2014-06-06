045d268e5959980f41ff985f54ef0aa76f3c8a05
andrelfs01 <andre.luisfs01@gmail.com>
2014-06-06 02:20:02 -0300

    correcao de bugs da versao anterior

--- Trabalho_Moveis/src/br/trabalho_moveis/Login_Activity.java
+++ Trabalho_Moveis/src/br/trabalho_moveis/Login_Activity.java
@@ -93,9 +93,9 @@ public class Login_Activity extends Activity {
 	}
 
 	protected void abrirTelaVotacao() {
-		Intent intent = new Intent(this, Votar_Activity.class);
-		intent.putExtra("itens", array);
-		startActivity(intent);
+	//	Intent intent = new Intent(this, Votar_Activity.class);
+		//intent.putExtra("itens", array);
+		//startActivity(intent);
 		//é só isso msm?
 	}
 
