c37226550bdbb96765811cc996b8863739199f26
andrelfs01 <andre.luisfs01@gmail.com>
2014-06-09 19:42:54 -0300

    Criando tela de votação

--- Trabalho_Moveis/src/br/trabalho_moveis/VotarFragment.java
+++ Trabalho_Moveis/src/br/trabalho_moveis/VotarFragment.java
@@ -1,21 +1,29 @@
 package br.trabalho_moveis;
 
-
-
+import br.model.Item;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-
-public class VotarFragment extends Activity{
+public class VotarFragment extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_votar_frag);
-		
-		//receber um string para colocar no textview
-		
+		Intent intent = getIntent();
+		if (intent != null) {
+			Bundle params = intent.getExtras();
+			if (params != null) {
+				Item i = new Item(params.getString("id_plan"), params.getString("nome"));
+				i.setId(params.getInt("id"));
+				
+				// receber um string para colocar no textview
+				TextView tv = (TextView) findViewById(R.id.textView_votar);
+				tv.setText(i.getNome());
+
+			}
+		}
 	}
-	
-	
-	
+
 }
